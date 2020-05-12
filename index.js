999fd0d24e6c5ff7d1f60aedec2a4be259abc2cf
Bilkis Ismail <bilkisismail07@gmail.com>
2020-05-12 20:06:20 +0700

    standard index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const { APP_PORT } = process.env
+const express = require('express')
+const app = express()
+
+// Setting up bodyParser to use json and set it to req.body
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+// Default Route
+app.get('/', (req, res) => {
+  res.send('Welcome to Kuma Backend')
+})
+
+// Error Route
+app.get('*', (req, res) => {
+  res.status(400).send('Pages not found')
+})
+
+app.listen(APP_PORT || 3000, () => {
+  console.log('Server run on port 3000')
+})
