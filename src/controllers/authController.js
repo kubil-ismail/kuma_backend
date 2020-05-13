199ec1167e3e722deaca293648ff68d46a1a2716
Bilkis Ismail <bilkisismail07@gmail.com>
2020-05-14 05:10:47 +0700

    add admin register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { APP_KEY } = process.env
+const { APP_KEY, APP_PIN } = process.env
 const authModel = require('../models/authModel')
 const bcrypt = require('bcryptjs')
 const salt = bcrypt.genSaltSync(10)
@@ -16,33 +16,60 @@ module.exports = {
         // Create Api Key
         jwt.sign({ _result }, APP_KEY, (err, token) => {
           if (!err) {
-            res.status(200).send({ status: true, message: 'Login successful', apiKey: token })
+            res.status(200).send({
+              status: true,
+              message: 'Login successful',
+              data: {
+                apiKey: token, role: _result.role_id
+              }
+            })
           } else {
-            res.status(400).send({ status: false, message: 'Unable to sign in at this time, try for a few moments' })
+            res.status(400).send({
+              status: false,
+              message: 'Unable to sign in at this time, try for a few moments'
+            })
           }
         })
       } else {
-        res.status(400).send({ status: false, message: 'Password do not match' })
+        res.status(400).send({
+          status: false,
+          message: 'Password do not match'
+        })
       }
-    }).catch(_ => res.status(400).send({ status: false, message: 'Email not registered' }))
+    }).catch(_ => res.status(400).send({
+      status: false,
+      message: 'Email not registered'
+    }))
   },
   registerAuth: (req, res) => {
-    const { email, password } = req.body
-    const checkEmail = authModel.findEmail({ email: email })
+    const { email, password, pin } = req.body
+    const checkPin = bcrypt.compareSync(pin, APP_PIN)
 
+    const checkEmail = authModel.findEmail({ email: email })
     checkEmail.then(_result => {
       if (!_result) {
         const data = {
           email: email,
-          password: bcrypt.hashSync(password, salt)
+          password: bcrypt.hashSync(password, salt),
+          role_id: checkPin ? 2 : 1
         }
         const createUser = authModel.createUser(data)
         createUser.then(_result => {
-          res.status(200).send({ status: true, message: 'Registration successful', userId: _result.insertId })
+          res.status(200).send({
+            status: true,
+            message: 'Registration successful',
+            userId: _result.insertId
+          })
         })
       } else {
-        res.status(400).send({ status: false, message: 'Email already registered' })
+        res.status(400).send({
+          status: false,
+          message: 'Email already registered'
+        })
       }
-    }).catch(_ => res.status(400).send({ status: false, message: 'Cant Sign In Right Now' }))
+    }).catch(_ => res.status(400).send({
+      status: false,
+      message: 'Cant Sign In Right Now'
+    }))
   }
 }
