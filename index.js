4a8a08d989dc05cf92747bb9703f65b3fe2dcc41
Bilkis Ismail <bilkisismail07@gmail.com>
2020-06-18 04:23:38 +0700

    add filter

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 require('dotenv').config()
-// const { APP_PORT } = process.env
+const { APP_PORT } = process.env
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const resData = require('./src/helper/response')
-const serverless = require('serverless-http')
 
 // CORS All
-app.use(cors())
+// app.use(cors())
 
 // Allowed Url Origins
-// const allowedOrigins = ['http://127.0.0.1:500']
+const allowedOrigins = ['http://localhost:3000']
 
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     // (like mobile apps or curl requests)
-//     if (!origin) return callback(null, true); if (allowedOrigins.indexOf(origin) === -1) {
-//       const msg = 'The CORS policy for this site does not ' +
-//         'allow access from the specified Origin.'
-//       return callback(new Error(msg), false)
-//     } return callback(null, true)
-//   }
-// }))
+app.use(cors({
+  origin: function (origin, callback) {
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true); if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.'
+      return callback(new Error(msg), false)
+    } return callback(null, true)
+  }
+}))
 
 // Setting up bodyParser to use json and set it to req.body
 app.use(bodyParser.json())
@@ -31,8 +30,8 @@ app.use(bodyParser.urlencoded({
 }))
 
 // Set static folder root
-app.use('/.netlify/book/cover/', express.static('public/assets/cover'))
-app.use('/.netlify/profile/cover/', express.static('public/assets/profile'))
+app.use('/book/cover/', express.static('public/assets/cover'))
+app.use('/profile/cover/', express.static('public/assets/profile'))
 
 // Import Routes
 const home = require('./src/routes/index')
@@ -45,17 +44,17 @@ const favoriteBooks = require('./src/routes/api/book/favoriteRoutes')
 const profileUser = require('./src/routes/api/user/profileRoutes')
 const sosmedUser = require('./src/routes/api/user/sosmedRoutes')
 
-app.use('/.netlify/', home) // Home Route
-app.use('/.netlify/auth', auth) // Auth Route
+app.use('/', home) // Home Route
+app.use('/auth', auth) // Auth Route
 
-app.use('/.netlify/author', authorBooks) // Book Authors Route
-app.use('/.netlify/book', books) // Books Route
-app.use('/.netlify/genre', genreBooks) // Book Genres Route
-app.use('/.netlify/review', reviewBooks) // Book Reviews Route
-app.use('/.netlify/favorite', favoriteBooks) // Book Reviews Route
+app.use('/author', authorBooks) // Book Authors Route
+app.use('/book', books) // Books Route
+app.use('/genre', genreBooks) // Book Genres Route
+app.use('/review', reviewBooks) // Book Reviews Route
+app.use('/favorite', favoriteBooks) // Book Reviews Route
 
-app.use('/.netlify/profile', profileUser) // Profile User Route
-app.use('/.netlify/sosmed', sosmedUser) // Sosmed User Route
+app.use('/profile', profileUser) // Profile User Route
+app.use('/sosmed', sosmedUser) // Sosmed User Route
 
 // Error Route
 app.get('*', (req, res) => {
@@ -64,7 +63,6 @@ app.get('*', (req, res) => {
   ))
 })
 
-module.exports.handler = serverless(app)
-// app.listen(APP_PORT || 8000, () => {
-//   console.log('Server run on port 3000')
-// })
+app.listen(APP_PORT || 8000, () => {
+  console.log('Server run on port 3000')
+})
